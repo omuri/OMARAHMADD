bf23f7641fb256dc0e2218c4a1626e0a48d178c2
omuri <omurii95@gmail.com>
2019-12-03 11:19:11 +0100

    Added sub method to the math class

--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -3,9 +3,14 @@ package com.company;
 public class Math {
     public static void main(String[] args) {
     }
-    public int add (int first, int second) {
+
+    public int add(int first, int second) {
         return first + second;
 
 
-}
+    }
+
+    public int sub(int first, int second) {
+        return first - second;
+    }
 }
